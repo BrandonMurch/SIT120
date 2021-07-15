e2cc7f3fadb5bc716b9ade16dcb136d945e85d1c
Brandon Murch <bmurch@deakin.edu.au>
2021-07-15 13:50:58 +1000

    Practical Week 1 - Task 1 and 2

---
+++ Practical 1/task3.js
@@ -0,0 +1,9 @@
+function submitForm(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+    const location = document.getElementById("location").value;
+    const tableBody = document.getElementById("table-body");
+
+    tableBody.innerHTML += `<tr><td>${name}</td><td>${age}</td><td>${location}</td></tr>`
+}
