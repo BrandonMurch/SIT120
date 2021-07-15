f48618887954dc50361cb265348b002c5652dc9f
Brandon Murch <bmurch@deakin.edu.au>
2021-07-16 09:08:34 +1000

    Change todo component and add task functionality

--- Practical 1/task4/index.js
+++ Practical 1/task4/index.js
@@ -4,17 +4,38 @@
 window.onload = function() {
     Vue.component('todo-item', {
         props: ['todo'],
-        template: '<li>{{ todo.text }}</li>'
+        template: `<li>
+        <input 
+            type="checkbox" 
+            :id="todo.key"  
+            v-model="todo.complete"
+        >
+        <label for="key">{{ todo.text }}</label>
+        </li>`
     })
+    
 
     var app = new Vue({
         el: '#todo-app',
-        data: {
-            groceryList: [
-                {id: 0, text: 'Vegetables', complete: true},
-                {id: 1, text: 'Cheese', complete: true},
-                {id: 2, text: 'Milk', complete: false},
-            ], 
+        data() {
+            return {
+                newToDoText : '',
+                groceryList: [
+                    {id: "task-0", text: 'Vegetables', complete: true},
+                    {id: "task-1", text: 'Cheese', complete: true},
+                    {id: "task-2", text: 'Milk', complete: false},
+                ], 
+            }
+        },
+        methods: {
+            addTask() {
+                this.groceryList.push({
+                    id: `task-${this.groceryList.length}`,
+                    text: this.newToDoText,
+                    complete: false,
+                })
+                this.newToDoText = '';
+            }
         }
     });
 }
