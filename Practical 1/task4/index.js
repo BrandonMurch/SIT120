747a67c80f2b63a36c2af234e93affe94b82d531
Brandon Murch <bmurch@deakin.edu.au>
2021-07-16 12:38:31 +1000

    Add link to Vue guide

--- Practical 1/task4/index.js
+++ Practical 1/task4/index.js
@@ -1,17 +1,23 @@
+/* 
+    Using the Vue.js guide located: 
+    https://v3.vuejs.org/guide/introduction.html#what-is-vue-js
+*/
+
 
 // window.onload is used to make sure the DOM loads before VUE.
 // This solution is modified from https://stackoverflow.com/questions/29484431/vue-warn-cannot-find-element
 window.onload = function() {
     Vue.component('todo-item', {
         props: ['todo'],
-        template: `<li>
-        <input 
-            type="checkbox" 
-            :id="todo.key"  
-            v-model="todo.complete"
-        >
-        <label for="key">{{ todo.text }}</label>
-        </li>`
+        template: `
+            <li>
+                <input 
+                    type="checkbox" 
+                    :id="todo.key"  
+                    v-model="todo.complete"
+                >
+                <label for="key">{{ todo.text }}</label>
+            </li>`
     })
     
 
