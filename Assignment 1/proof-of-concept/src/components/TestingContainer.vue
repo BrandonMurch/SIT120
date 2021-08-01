e4bbc11295241b81a9c34415773b877d5904d56b
Brandon Murch <bmurch@deakin.edu.au>
2021-08-01 16:25:28 +1000

    Add background to webpage

--- Assignment 1/proof-of-concept/src/components/TestingContainer.vue
+++ Assignment 1/proof-of-concept/src/components/TestingContainer.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>
+  <div>
+      <div class="application-background"></div>
       <NavigationBar />
       <ExplorePlants />
-    </div>
+  </div>
 </template>
 
 <script>
@@ -14,6 +15,17 @@ export default {
   components: {
       NavigationBar,
       ExplorePlants,
-  }
+  },
 }
 </script>
+
+<style scoped>
+  .application-background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: #D8DAD4;
+  }
+</style>
