15d2e087aa99ede06357653534c2b99c205a644f
Brandon Murch <bmurch@deakin.edu.au>
2021-08-01 15:03:17 +1000

    Create image gallery.

--- Assignment 1/proof-of-concept/src/components/ExplorePlants.vue
+++ Assignment 1/proof-of-concept/src/components/ExplorePlants.vue
@@ -1,7 +1,6 @@
 <template>
-    <div>
-        <!-- <div v-for="image in images" v-bind:key="image.hoverText"></div> -->
-        <ImageGallery v-bind:images="imageData"/>
+    <div class="image-gallery">
+        <ImageGallery v-bind:images="getImageData()"/>
     </div>
 </template>
 
@@ -10,16 +9,30 @@ import ImageGallery from './ImageGallery.vue'
 
 export default {
     name: 'ExplorePlants', 
-    data() {
-        return {
-            imageData: [
-                {
+    methods: {
+        getImageData() {
+            const images = [];
+            for (let i = 0; i < 5; i++) {
+                images.push({
                     imageSource: "https://images.unsplash.com/photo-1517848568502-d03fa74e1964?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
                     hoverText: "Monstera"
-                }
-            ]
+                });
+                images.push({
+                    imageSource: "https://images.unsplash.com/photo-1497250681960-ef046c08a56e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+                    hoverText: "Fern"
+                });
+                images.push({
+                    imageSource: "https://images.unsplash.com/photo-1453904300235-0f2f60b15b5d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1833&q=80",
+                    hoverText: "Plant"
+                }),
+                images.push({
+                    imageSource: "https://images.unsplash.com/photo-1508502726440-477c94bc369e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+                    hoverText: "Plant2"
+                });
+            }
+            return images;
         }
-    }, 
+    },
     components: {
         ImageGallery
     }
@@ -27,4 +40,10 @@ export default {
 </script>
 
 <style>
+    .image-gallery {
+        display: flex;
+        width: 100vw;
+        height: 100vh;
+        align-content: center;
+    }
 </style>
--- Assignment 1/proof-of-concept/src/components/ImageGallery.vue
+++ Assignment 1/proof-of-concept/src/components/ImageGallery.vue
@@ -1,9 +1,11 @@
 <template>
-    <div  class="gallery-container">
-        <ImageCard 
-            v-for="image in images" 
-            v-bind:key="image.hoverText" 
-            v-bind="image" />
+    <div  class="gallery-container" v-bind:style="centerContainer">
+        <div class="gallery-column" v-for="(column, index) in imageColumns" :key="index">
+            <ImageCard 
+                v-for="image in column" 
+                v-bind:key="image.hoverText" 
+                v-bind="image" />
+        </div>        
     </div>
 </template>
 
@@ -17,14 +19,50 @@ export default {
     },
     props: {
         images: Array
-    }
+    },
+    //https://stackoverflow.com/questions/49380830/vue-js-how-to-get-window-size-whenever-it-changes
+    created() {
+        window.addEventListener("resize", this.updateColumnsOnResize)
+    },
+    unmounted() {
+        window.removeEventListener("resize", this.updateColumnsOnResize)
+    },
+    methods: {
+        updateColumnsOnResize() {
+            this.imageColumns = this.splitImages();
+        },
+        splitImages() {
+            const columns = Math.floor(window.innerWidth / 265)
+            const columnQueue = [];
+            for (let i = 0; i < columns; i++) {
+                columnQueue[i] = [];
+            }
+            for (let i = 0; i < this.images.length; i++) {
+                columnQueue[i % columns].push(this.images[i]);
+            }
+            return columnQueue;
+        }
+    },
+    data () {
+        return {
+            imageColumns: this.splitImages(),
+        }
+    },
+
 }
 </script>
 
 <style scoped>
     .gallery-container {
-        position: absolute;
-        top: 90px;
-        left: 0;
+        position: relative;
+        top: 0;
+        margin: auto;
+        display: flex;
+        flex-direction: row;
     }
+
+    .gallery-column {
+        width: 250px;
+        margin: 0.5rem;
+}
 </style>
