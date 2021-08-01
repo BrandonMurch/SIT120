3b1bdded365823f3021a47830bcb865ef1235441
Brandon Murch <bmurch@deakin.edu.au>
2021-08-01 22:45:49 +1000

    Add drop-down menu for navigation

---
+++ Assignment 1/proof-of-concept/src/components/DropDown.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div :class="{ open: isOpen, container: true }">
+            <slot name="inside"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DropDown",
+    props: {
+        isOpen: Boolean,
+    }
+}
+</script>
+
+<style scoped>
+
+    .container {
+        position: relative;
+        left: -10%;
+        max-height: 0;
+        width: 120%;
+        overflow: hidden;
+        transition: 1s;
+        transition-timing-function: ease;
+        transition-property: max-height;
+        box-shadow: inset 0px 0px 4px 4px grey;
+    }
+
+    .open {
+        height: auto;
+        max-height: 800px;
+        transition: 1s;
+        transition-timing-function: ease;
+        transition-property: max-height;
+        border-top: 1px solid black;
+        border-bottom: 1px solid black;
+    }
+
+
+</style>
--- Assignment 1/proof-of-concept/src/components/NavigationBar.vue
+++ Assignment 1/proof-of-concept/src/components/NavigationBar.vue
@@ -2,18 +2,47 @@
     <nav class="navigation-bar">
         <img class="logo-image" src="../assets/images/logo.svg" alt="logo">
         <h1 class="logo-text">tend</h1>
-        <a class="navigation-link" href="#">contact</a>
-        <a class="navigation-link" href="#">search</a>
-        <a class="navigation-link" href="#">learn</a>
-        <a class="navigation-link" href="#">explore</a>
-        <a class="navigation-link" href="#">my plants</a>
-        
+        <div class="icon-container" @click="toggle">
+            <img v-if="!isOpen" class="menu-icon" src="@/assets/images/MenuIcon.svg" />
+            <!-- <img v-else class="menu-icon" src="@/assets/icons/close.svg" /> -->
+            <svg v-else class="menu-icon" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><path d="M38 12.83L35.17 10 24 21.17 12.83 10 10 12.83 21.17 24 10 35.17 12.83 38 24 26.83 35.17 38 38 35.17 26.83 24z"/></svg>
+        </div>
+        <DropDown class="drop-down" :isOpen="isOpen"> 
+            <template v-slot:inside> 
+                <a class="navigation-link drop-link" v-for="link in links" :key="link.text" :href="link.url">{{ link.text }}</a>
+            </template>
+        </DropDown>
+        <div class="large-navigation">
+            <a class="navigation-link" v-for="link in links" :key="link.text" :href="link.url">{{ link.text }}</a>
+        </div>
     </nav>
 </template>
 
 <script>
+import DropDown from './DropDown.vue'
+
 export default {
     name: 'NavigationBar', 
+    components: {
+        DropDown
+    },
+    data() {
+        return {
+            isOpen: false,
+            links: [
+                {text: "contact", url: "#"},
+                {text: "search", url: "#"},
+                {text: "learn", url: "#"},
+                {text: "explore", url: "#"},
+                {text: "my plants", url: "#"},
+            ]
+        }
+    }, 
+    methods: {
+        toggle() {
+            this.isOpen = !this.isOpen;
+        }
+    }
 }
 </script>
 
@@ -23,7 +52,7 @@ export default {
         top: 0;
         left: 0;
         width: 100vw;
-        height: 2rem;
+        min-height: 2rem;
     }
 
     .logo-image {
@@ -46,11 +75,44 @@ export default {
         font-size: 1.5rem;
     }
 
-    .navigation-link:first-of-type {
-        margin-right: 3rem;
+    .drop-link {
+        width: 100%;
+        text-align: center;
+        border-bottom: 2px solid grey;
+        margin: 0;
+        padding: 1rem;
     }
 
     .navigation-link:hover {
         font-weight: bold;
     }
+
+    .menu-icon {
+        height: 4rem;
+        width: 4rem;
+        position: absolute;
+        right: 2rem;
+        top: 1rem;
+        cursor: pointer;
+        fill: #BF763C;
+    }
+
+    @media (min-width: 800px) {
+        .drop-down, .icon-container {
+            display: none;
+        }
+
+        .large-navigation {
+            position: absolute;
+            top: 1rem;
+            right: 2rem;
+        }
+    }
+
+    @media (max-width: 800px) {
+        .large-navigation {
+            display: none;
+        }
+    }
+
 </style>
