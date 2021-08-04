6223a6a20c6a194f57113e659ca0b5a160a3714c
Brandon Murch <bmurch@deakin.edu.au>
2021-08-04 15:54:15 +1000

    Clean up and reformat

--- Assignment 1/proof-of-concept/src/components/SearchBar.vue
+++ Assignment 1/proof-of-concept/src/components/SearchBar.vue
@@ -1,69 +1,78 @@
 <template>
-  <DropDown :isOpen="isOpen">
-        <template v-slot:inside >
-            <div class="search-container" ref="searchbar">
-                <input class="search-input" :key="isOpen" type="text" placeholder="What are you looking for?" v-model="searchTerm">
-            </div>
-        </template>
-    </DropDown>
+	<DropDown :isOpen="isOpen">
+		<template v-slot:inside>
+			<div class="search-container" ref="searchbar">
+				<input
+					class="search-input"
+					:key="isOpen"
+					type="text"
+					placeholder="What are you looking for?"
+					v-model="searchTerm"
+				/>
+			</div>
+		</template>
+	</DropDown>
 </template>
 
 <script>
-import DropDown from './DropDown.vue'
+import DropDown from "./DropDown.vue";
 
 export default {
-    name: "SearchBar",
-    components: { DropDown },
-    props: { isOpen: Boolean },
-    watch: {
-        isOpen: function(value) {
-            if (!value) {
-                this.close();
-            }
-        }
-    },
-    data() {
-        return {
-            searchTerm: "",
-        }
-    }, methods: {
-        close() {
-            this.searchTerm = "";
-            this.$emit("close");
-        }, submitSearch(event) {
-            if (event.key == "Enter") {
-                alert("Searching for: " + this.searchTerm + "...");
-                this.close()
-            } else if (event.key == "Escape") {
-                this.close();
-            }
-        }
-    },
-    created() {
-        window.addEventListener("keyup", this.submitSearch);
-    }, unmounted () {
-        window.addEventListener("keyup", this.submitSearch);
-    }
-}
+	name: "SearchBar",
+	components: { DropDown },
+	props: { isOpen: Boolean },
+	watch: {
+		isOpen: function(value) {
+			if (!value) {
+				this.close();
+			}
+		},
+	},
+	data() {
+		return {
+			searchTerm: "",
+		};
+	},
+	methods: {
+		close() {
+			this.searchTerm = "";
+			this.$emit("close");
+		},
+		submitSearch(event) {
+			if (event.key == "Enter") {
+				alert("Searching for: " + this.searchTerm + "...");
+				this.close();
+			} else if (event.key == "Escape") {
+				this.close();
+			}
+		},
+	},
+	created() {
+		window.addEventListener("keyup", this.submitSearch);
+	},
+	unmounted() {
+		window.addEventListener("keyup", this.submitSearch);
+	},
+};
 </script>
 
 <style scoped>
-   .search-container {
-        width: 100%;
-        height: 3.4rem;
-        display: block;
-    }
+.search-container {
+	width: 100%;
+	height: 3.4rem;
+	display: block;
+}
 
-    .search-input {
-        position: relative;
-        width: 100%;
-        background-color: rgba(0, 0, 0, 0.1);
-        border: none;
-        height: 2rem;
-        text-align: center;
-        color:white;
-        font-family: fell, serif;
-        font-size: 2rem;
-        padding: 1rem;
-    }
+.search-input {
+	position: relative;
+	width: 100%;
+	background-color: rgba(0, 0, 0, 0.05);
+	border: none;
+	height: 2rem;
+	text-align: center;
+	color: #3f463d;
+	font-family: fell, serif;
+	font-size: 2rem;
+	padding: 1rem;
+}
 </style>
